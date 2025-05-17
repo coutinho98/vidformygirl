bb65a11b2e66b11826ee9db23a16bd35716ccd83
coutinho98 <mateuscouto98@outlook.com>
2025-05-17 03:29:15 -0300

    feat: url validation added

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from yt_dlp import YoutubeDL
+from urllib.parse import urlparse
 
 url = input(": ")
+
+if not url.startswith(('http://', 'https://')) or 'youtube' not in urlparse(url).netloc:
+    print('URL inválida ou não é do YouTube')
+    exit(1)
+
 ydl_opts = {
     'format': 'bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[vcodec^=avc1]',
     'outtmpl': '%(title)s.%(ext)s',
