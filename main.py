b17458307a75ae9619e1659422f8abc6b4ee4941
coutinho98 <mateuscouto98@outlook.com>
2025-05-17 03:32:54 -0300

    feat: added download progression

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ ydl_opts = {
         'key': 'FFmpegVideoConvertor',
         'preferedformat': 'mkv',
     }],
+    'progress_hooks': [lambda d: print(f"Progresso: {d['downloaded_bytes'] / d['total_bytes'] * 100:.1f}%") if d['status'] == 'downloading' else None],
 }
 
 try:
     with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        print(f"baixado: {info['title']} em {info.get('resolução', 'bad resolution')}")
+        print(f"baixado: {info['title']} em {info.get('height', 'resolução desconhecida')}p")
 except Exception as e:
     print(f"Error: {e}")
